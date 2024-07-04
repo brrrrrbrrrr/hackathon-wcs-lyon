231ef9033b265aae4b15996b10a348fb845bf24c
Salma DESMAZIERES <s.desmazieres@yahoo.fr>
2024-07-04 10:46:57 +0200

    creation des différentes pages et des routes. Creation du composant header et footer

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css';
+import { Outlet, useLocation } from 'react-router-dom';
+import Header from '../src/components/Header/Header.jsx'
+import Footer from '../src/components/Footer/Footer.jsx'
 import Prompt from './components/Prompt.jsx';
 import Questions from './components/Questions.jsx';
 import Record from './components/Record.jsx';
 
 function App() {
+
+  const location = useLocation();
+  const hideHeaderFooter =
+    ["/quizz"].includes(location.pathname) === true;
+
   return (
     <>
+      {hideHeaderFooter === false && <Header />}
+      <main>
+        <Outlet />
+      </main>
+      {hideHeaderFooter === false && <Footer />}
       <Prompt />
       <Questions />
       <Record />
