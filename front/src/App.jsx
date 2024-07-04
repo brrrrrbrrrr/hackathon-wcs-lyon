6c8e2b75c3a621d87cde0296fb68b9e7391444ad
Benjamin CHAILLAN <benjamin.chaillan-pro@laposte.net>
2024-07-04 12:31:02 +0200

    burger done

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <>
       {hideHeaderFooter === false && <Header />}
       <main>
-        <BurgerMenu />
+        {/* <BurgerMenu /> */}
         <Outlet />
       </main>
       {hideHeaderFooter === false && <Footer />}
